5f24fe8016c8596d244c6dd539b7007f7c431130
fmfahath <fmfahath.dev@gmail.com>
2023-03-03 15:50:45 +0530

    menu-items.js component added

---
+++ src/components/menu-items.js
@@ -0,0 +1,31 @@
+export const menueItems = [
+    {
+        title: "Home",
+        url: "/",
+        clsName:"nav-links",
+        icon: "fa-solid fa-house"
+    },
+    {
+        title: "About",
+        url: "/about",
+        clsName:"nav-links",
+        icon: "fa-solid fa-circle-info"
+    },
+    {
+        title: "Services",
+        url: "/services",
+        clsName:"nav-links",
+        icon: "fa-solid fa-briefcase"
+    },
+    {
+        title: "Contact",
+        url: "/contact",
+        clsName:"nav-links",
+        icon: "fa-solid fa-address-book"
+    },
+    {
+        title: "Sign Up",
+        url: "/signup",
+        clsName:"nav-links-mobile",
+    },
+];
