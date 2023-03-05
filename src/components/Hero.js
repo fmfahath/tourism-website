720c4411baeee7a86aa31d089cd29ce60893c97e
fmfahath <fmfahath.dev@gmail.com>
2023-03-06 03:53:20 +0530

    Hero.js text/ img added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,6 +6,11 @@ function Hero(){
         <React.Fragment>
             <div className='hero'>
                 <img alt='hero-img' src="https://images.unsplash.com/photo-1464188776886-149f86bd1027?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
+
+                <div className="hero-text"></div>
+                <h1>Your Journy Your Story</h1>
+                <p>Choose Your Favourite</p>
+                <a href="/">Travel Plan</a>
             </div>
         </React.Fragment>
     )
