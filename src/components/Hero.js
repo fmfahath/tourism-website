111880ffeddc9d158fd8d1f5b1689aa5acb705ee
fmfahath <fmfahath.dev@gmail.com>
2023-03-06 04:03:04 +0530

    render Hero.js to Home.js using props

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,14 @@
 import React from "react";
 import './Hero-styles.css'
 
-function Hero(){
+function Hero(props){
     return(
         <React.Fragment>
-            <div className='hero'>
-                <img alt='hero-img' src="https://images.unsplash.com/photo-1464188776886-149f86bd1027?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
-
+            <div className={props.cName}>
+                <img alt='hero-img' src={props.heroImg}></img>
                 <div className="hero-text"></div>
-                <h1>Your Journy Your Story</h1>
-                <p>Choose Your Favourite</p>
+                <h1>{props.title}</h1>
+                <p>{props.text}</p>
                 <a href="/">Travel Plan</a>
             </div>
         </React.Fragment>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,12 @@ function Home(){
     return(
         <React.Fragment>
             <Navbar/>
-            <Hero/>
+            <Hero 
+                cName = "hero"
+                heroImg = "https://images.unsplash.com/photo-1501696461415-6bd6660c6742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+                title = "YourJourny, Your Storey"
+                text = "Choose Your Favourite dstination"
+            />
         </React.Fragment>
     )
 }
