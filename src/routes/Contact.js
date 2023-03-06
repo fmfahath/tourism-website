09aeab8990e71c2f1abf853b4c3df5947ea00944
fmfahath <fmfahath.dev@gmail.com>
2023-03-06 14:35:49 +0530

    Contact.js & Services.js hero sections completed

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Hero from "../components/Hero";
+import ContactImg from "../assets/2.jpg"
 
 function Contact(){
     return(
         <React.Fragment>
-            <Navbar/>
-            <h1>This is Contct Page</h1>
+            <Navbar/> 
+            <Hero 
+                cName = "hero-mid"
+                heroImg = {ContactImg}
+                title = "Contact Us"
+                text = ""
+                url="/"
+                buttonClass=""
+                buttonText=""
+            />
         </React.Fragment>
     )
 }
--- src/routes/Services.js
+++ src/routes/Services.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Hero from "../components/Hero";
+import ServicesImg from "../assets/3.jpg"
 
 function Services(){
     return(
         <React.Fragment>
-            <Navbar/>
-            <h1>This is Services Page</h1>
+             <Navbar/> 
+            <Hero 
+                cName = "hero-mid"
+                heroImg = {ServicesImg}
+                title = "Our Services"
+                text = ""
+                url="/"
+                buttonClass=""
+                buttonText=""
+            />
         </React.Fragment>
     )
 }
