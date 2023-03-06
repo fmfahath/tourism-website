2e75e2d554d16af30b6dd81d34bf1daf1dffceea
fmfahath <fmfahath.dev@gmail.com>
2023-03-06 14:25:13 +0530

    About.js hero section completed

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Hero from "../components/Hero";
 import Navbar from "../components/navbar";
+import AboutImg from "../assets/night.jpg" //creating variable for the hero image
 
 function About(){
     return(
         <React.Fragment>
             <Navbar/>
-            <h1>This is About Page</h1>
+            <Hero 
+                cName = "hero-mid"
+                heroImg = {AboutImg}
+                title = "About Us"
+                text = ""
+                url="/"
+                buttonClass=""
+                buttonText=""
+            />
         </React.Fragment>
     )
 }
