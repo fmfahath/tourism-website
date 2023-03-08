647e69d76fd537246799b863685a37e95574caf3
fmfahath <fmfahath.dev@gmail.com>
2023-03-08 15:56:57 +0530

    TripData.js component's props assigned

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -3,7 +3,13 @@ import "./Trip-styles.css"
 
 function Trip(){
     return(
-        
+        <div className="t-card">
+            <div className="t-image">
+                <img alt="trip-image" src={props.image}></img>
+            </div>
+            <h4>{props.heading}</h4>
+            <p>{props.text}</p>
+        </div>
     )
 }
 
--- src/components/TripData.js
+++ src/components/TripData.js
@@ -2,7 +2,13 @@ import "./Trip-styles.css"
 
 function TripData(){
     return(
-
+        <div className="t-card">
+            <div className="t-image">
+                <img alt="trip-image" src={props.image}></img>
+            </div>
+            <h4>{props.heading}</h4>
+            <p>{props.text}</p>
+        </div>
     )
 }
 
