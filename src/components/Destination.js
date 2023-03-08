ec30bc8fc8697168f8ae93fee55137eb80539d4d
fmfahath <fmfahath.dev@gmail.com>
2023-03-08 15:29:02 +0530

    Hero imge changed

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -1,9 +1,9 @@
 import "./destination-styles.css"
 import DestinationData from "./DestinationData"
-import Mountain1 from "../assets/1.jpg"
-import Mountain2 from "../assets/2.jpg"
-import Mountain3 from "../assets/3.jpg"
-import Mountain4 from "../assets/4.jpg"
+import Image1 from "../assets/1.jpg"
+import Image2 from "../assets/2.jpg"
+import Image3 from "../assets/3.jpg"
+import Image4 from "../assets/5.jpg"
 
 
 const Destination = () => {
@@ -14,7 +14,7 @@ const Destination = () => {
 
             <DestinationData
                 className="first-des"
-                heading="Taal Volcano, batangas"
+                heading="Taal Volcano, Batangas"
                 text="Taal, located in the Philippines, is part of a caldera system in southern Luzon Island. Recorded eruptions 
                 date back to 3,580 BCE. The caldera is 15 x 20 km and contains Lake Taal, which has a depth of 160 m, and 
                 an island that also contains a lake within the Main Crater. Eruptive activity during 1-9 July 2021 was 
@@ -22,22 +22,24 @@ const Destination = () => {
                 characterized by phreatomagmatic explosions, gas-and-steam emissions, including sulfur dioxide emissions, 
                 and seismicity (BGVN 46:10). This report describes activity during October 2021-March 2022 using reports 
                 from the Philippine Institute of Volcanology and Seismology (PHIVOLCS) and satellite data"
-                img1={Mountain1}
-                img2={Mountain2}
+                img1={Image1}
+                img2={Image2}
             />
 
             <DestinationData
                 className="first-des-reverse"
-                heading="Taal Volcano, batangas"
-                text="Taal, located in the Philippines, is part of a caldera system in southern Luzon Island. Recorded eruptions 
-                date back to 3,580 BCE. The caldera is 15 x 20 km and contains Lake Taal, which has a depth of 160 m, and 
-                an island that also contains a lake within the Main Crater. Eruptive activity during 1-9 July 2021 was 
+                heading="Eiffel Tower, Paris"
+                text="Eiffel Tower, French Tour Eiffel, Parisian landmark that is also a technological masterpiece in 
+                building-construction history. When the French government was organizing the International Exposition 
+                of 1889 to celebrate the centenary of the French Revolution, a competition was held for designs for a 
+                suitable monument. 
                 
-                characterized by phreatomagmatic explosions, gas-and-steam emissions, including sulfur dioxide emissions, 
-                and seismicity (BGVN 46:10). This report describes activity during October 2021-March 2022 using reports 
-                from the Philippine Institute of Volcanology and Seismology (PHIVOLCS) and satellite data"
-                img1={Mountain3}
-                img2={Mountain4}
+                More than 100 plans were submitted, and the Centennial Committee accepted that of 
+                the noted bridge engineer Gustave Eiffel. Eiffel’s concept of a 300-metre (984-foot) tower built almost 
+                entirely of open-lattice wrought iron aroused amazement, skepticism, and no little opposition on aesthetic 
+                grounds. When completed, the tower served as the entrance gateway to the exposition"
+                img1={Image3}
+                img2={Image4}
             />
         </div>
     )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Destination from "../components/Destination";
 import Hero from "../components/Hero";
 import Navbar from "../components/navbar";
+import HeroImage from "../assets/12.jpg"
 
 function Home(){
     return(
@@ -9,7 +10,7 @@ function Home(){
             <Navbar/>
             <Hero 
                 cName = "hero"
-                heroImg = "https://images.unsplash.com/photo-1501696461415-6bd6660c6742?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+                heroImg = {HeroImage}
                 title = "Your Journy, Your Story"
                 text = "Choose Your Favourite Detination"
                 url="/"
