6a4757d627171e7cc062d1ce00afeddeb3b76db5
fmfahath <fmfahath.dev@gmail.com>
2023-03-08 15:53:21 +0530

    Trip.js , TripData.js, Trip-styles.css component added

---
+++ src/components/Trip.js
@@ -0,0 +1,10 @@
+import "./Trip-styles.css"
+
+
+function Trip(){
+    return(
+        
+    )
+}
+
+export default Trip;
---
+++ src/components/TripData.js
@@ -0,0 +1,9 @@
+import "./Trip-styles.css"
+
+function TripData(){
+    return(
+
+    )
+}
+
+export default TripData;
