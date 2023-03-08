76a012fa20df33dde3fb2e3e1b016a71c8d17b19
fmfahath <fmfahath.dev@gmail.com>
2023-03-08 13:49:52 +0530

    DestinationData.js, DestinationData-styles.css components created

---
+++ src/components/DestinationData.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+class DestinationData extends Component{
+    render(){
+        return(
+            <h1>DestinationData.js</h1>
+        )
+    }
+}
+
+export default DestinationData;
